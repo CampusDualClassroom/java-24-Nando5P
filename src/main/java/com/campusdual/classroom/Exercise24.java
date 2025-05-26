8a462c733931b38d60ae3794ec1a2cd719a248e8
Nando Parga <50nando05@gmail.com>
2025-05-26 08:49:28 +0200

    Exercise24 Resuelto

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -5,14 +5,27 @@ import java.util.Queue;
 
 public class Exercise24 {
     public static Queue<String> createQueue() {
-
+        Queue<String> lista = new LinkedList<>();
+        lista.offer("Smith");
+        lista.offer("Montessori");
+        lista.offer("Peralta");
+        lista.offer("House");
+        return lista;
     }
-    public static void printAndEmptyQueue(Queue<String> queue) {
 
+    public static void printAndEmptyQueue(Queue<String> queue) {
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
     }
 
     public static void main(String[] args) {
+        Queue<String> queue = createQueue();
+        System.out.println("Contenido de la cola:");
+        printAndEmptyQueue(queue);
 
+        System.out.println("Cola despues de vaciarse:");
+        printAndEmptyQueue(queue); // No debería imprimir nada, ya que la cola está vacía.
     }
 
 }
